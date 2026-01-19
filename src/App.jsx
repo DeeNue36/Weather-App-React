f32fa656df17a0dec4f4f3916406b675f521cfda
Dugiii <dugidemeg@gmail.com>
2026-01-18 22:00:24 -0600

    app layout and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
+import { Header } from './components/Header'
+import { Search } from './components/Search'
 import './App.css'
 
 function App() {
 
   return (
     <>
-
+      <main className="container">
+        <Header />
+        <h1>How's the sky looking today?</h1>
+        <Search />
+      </main>
     </>
   )
 }

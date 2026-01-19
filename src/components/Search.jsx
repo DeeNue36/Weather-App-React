b86c10c876416233a3493eeddabbb05ff6a195ff
Dugiii <dugidemeg@gmail.com>
2026-01-18 21:59:51 -0600

    created components

---
+++ src/components/Search.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Search = ({ searchQuery, setSearchQuery }) => {
+    return (
+        <div className="search">
+            <div className="search-input">
+                <img src="icon-search.svg" alt="Search icon" />
+
+                <input
+                    type="text"
+                    placeholder='Search for a place...'
+                    value={searchQuery}
+                    // onChange={(e) => setSearchQuery(e.target.value)}
+                />
+
+            </div>
+            <button className="search-btn" onClick={setSearchQuery}>
+                Search
+            </button>
+        </div>
+    )
+}

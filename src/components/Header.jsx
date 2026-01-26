29533387667ad5b29c4ddbff26578680b8eb8431
Dugiii <dugidemeg@gmail.com>
2026-01-26 00:57:58 -0600

    removed react fragment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,15 @@ import React from 'react'
 
 export const Header = () => {
     return (
-        <>
-            <nav className="navbar">
-                <div className="logo">
-                    <img src="logo.svg" alt="Weather Now Logo" />
-                </div>
-                <button className="units-btn">
-                    <img src="icon-units.svg" alt="units icon" className="units-icon" />
-                    Units
-                    <img src="icon-dropdown.svg" alt="units dropdown icon" className="units-dropdown" />
-                </button>
-            </nav>
-        </>
+        <nav className="navbar">
+            <div className="logo">
+                <img src="logo.svg" alt="Weather Now Logo" />
+            </div>
+            <button className="units-btn">
+                <img src="icon-units.svg" alt="units icon" className="units-icon" />
+                Units
+                <img src="icon-dropdown.svg" alt="units dropdown icon" className="units-dropdown" />
+            </button>
+        </nav>
     )
 }

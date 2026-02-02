ab410ba9b9f5cca8d0b565c0d7e68acc307a51a9
Dugiii <dugidemeg@gmail.com>
2026-02-01 22:37:11 -0600

    lifted state to components

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -1,11 +1,18 @@
+import { useState } from 'react'
 import { Header } from './Header'
 import { WeatherData } from './WeatherData'
 
 export const WeatherApp = () => {
+    const [units, setUnits] = useState({
+        temperature: "celsius",
+        wind: "km/h",
+        precipitation: "mm"
+    });
+
     return (
         <div className='container'>
-            <Header />
-            <WeatherData />
+            <Header units={units} setUnits={setUnits} />
+            <WeatherData units={units}/>
         </div>
     )
 }

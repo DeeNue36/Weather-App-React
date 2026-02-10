faa98d316293034739b9eb9fa77f3bd8f93b6229
Dugiii <dugidemeg@gmail.com>
2026-02-10 01:05:08 -0600

    removed state and params

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -1,18 +1,11 @@
-import { useState } from 'react'
 import { Header } from './Header'
 import { WeatherData } from './WeatherData'
 
 export const WeatherApp = () => {
-    const [units, setUnits] = useState({
-        temperature: "celsius",
-        wind: "km/h",
-        precipitation: "mm"
-    });
-
     return (
         <div className='container'>
-            <Header units={units} setUnits={setUnits} />
-            <WeatherData units={units}/>
+            <Header />
+            <WeatherData />
         </div>
     )
 }

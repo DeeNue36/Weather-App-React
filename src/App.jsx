6954ccccd399c28f411ec55689a9d2d8dd5d7f68
Dugiii <dugidemeg@gmail.com>
2026-02-10 01:04:34 -0600

    Wrapped app with context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { WeatherApp } from './components/WeatherApp'
+import { UnitsProvider } from './context/UnitsContextProvider'
 import './App.css'
 
 function App() {
 
   return (
     <main>
-      <WeatherApp />
+      {/* Wrap the WeatherApp component with the Provider - now all its children/components can use UseUnitsContext() */}
+      <UnitsProvider> 
+        <WeatherApp />
+      </UnitsProvider>
     </main>
   )
 }

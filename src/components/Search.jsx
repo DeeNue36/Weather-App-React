f4b8f901f0c9a9e1bbd2b88ac531edd2a30aba92
Dugiii <dugidemeg@gmail.com>
2026-02-18 22:26:50 -0600

    loading state and code comments

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,13 +3,13 @@ import { BASE_CITY_API_URL } from '../api';
 import { Spinner } from './Spinner';
 
 export const Search = ({ searchCity, setSearchCity, isLoading, fetchWeatherData }) => {
-    const [citySuggestions, setCitySuggestions] = useState([]);
-    const [showSuggestedCities, setShowSuggestedCities] = useState(false);
-    const [isSearching, setIsSearching] = useState(false);
+    const [citySuggestions, setCitySuggestions] = useState([]); // Array of city suggestions
+    const [showSuggestedCities, setShowSuggestedCities] = useState(false); // state to control visibility of suggested cities
+    const [isSearching, setIsSearching] = useState(false); // state to control visibility of spinner
     const searchRef = useRef(null); // Reference to detect clicks outside dropdown
 
 
-    // Close dropdown when clicking outside it
+    // Close the search dropdown when clicking outside it
     useEffect(() => {
         const handleClickOutside = (e) => {
             if (searchRef.current && !searchRef.current.contains(e.target)) {
@@ -25,8 +25,11 @@ export const Search = ({ searchCity, setSearchCity, isLoading, fetchWeatherData 
 
 
     //? Fetch city search suggestions from API
-    const fetchCitySuggestions = async (query) => {
+    const fetchCitySuggestions = async (query) => { 
+        //* Reset states at the start of a new search
         setIsSearching(true);
+        setShowSuggestedCities(false);
+        setCitySuggestions([]);
 
         try {
             const endpoint = `${BASE_CITY_API_URL}name=${encodeURIComponent(query)}&count=4&language=en&format=json`;
@@ -42,6 +45,12 @@ export const Search = ({ searchCity, setSearchCity, isLoading, fetchWeatherData 
             if (data.results && data.results.length > 0) {
                 setCitySuggestions(data.results);
                 setShowSuggestedCities(true);
+
+                // simulate a delay before showing city suggestions
+                setTimeout(() => {
+                    setShowSuggestedCities(true);
+                    setIsSearching(false); // turn off loading after the delay of 2 seconds
+                }, 500);
             }
             else {
                 setCitySuggestions([]);
@@ -52,19 +61,22 @@ export const Search = ({ searchCity, setSearchCity, isLoading, fetchWeatherData 
             console.error('Error fetching city suggestions:', error);
             setCitySuggestions([]);
         }
-        finally {
-            setIsSearching(false);
-        }
+        // finally {
+        //     setIsSearching(false);
+        // }
     }
 
-    // Show weather data for a city suggestion that is clicked
+
+    //? Show weather data for a city suggestion that is clicked
     const handleCitySuggestionClick = (city) => {
         setSearchCity(city.name);
         fetchWeatherData(city.name, {latitude: city.latitude, longitude: city.longitude});
+        setIsSearching(false);
         setShowSuggestedCities(false); // Close dropdown
     };
 
-    // Handle Input Submission (Enter key or Search button click)
+
+    //? Handle Input Submission (Enter key or Search button click)
     const handleSearch = (e) => {
         e?.preventDefault(); // Prevent default form submission behavior
 
@@ -78,7 +90,8 @@ export const Search = ({ searchCity, setSearchCity, isLoading, fetchWeatherData 
         }
     };
 
-    // Handle Enter key - also shows dropdown
+
+    //? Handle Enter key - also shows dropdown
     const handleKeyDown = (e) => {
         if (e.key === 'Enter') {
             handleSearch(e);
@@ -103,41 +116,37 @@ export const Search = ({ searchCity, setSearchCity, isLoading, fetchWeatherData 
                         onChange = {(e) => setSearchCity(e.target.value)}
                         onKeyDown = {handleKeyDown}
                     />
-
-                    {/* Comment out later */}
-                    {/* {isSearching && (
-                        <div>
-                            <Spinner />
-                            <span className="search-loading">Search in progress</span>
-                        </div>
-                    )} */}
                 </div>
 
                 {/* Show suggested cities based on user search/query */}
                 {showSuggestedCities && citySuggestions.length > 0 && (
                     <ul className="searched-suggestions">
-                        {/* {isSearching && (
-                            <div>
-                                <Spinner />
-                                <span className="search-loading">Search in progress</span>
-                            </div>
-                        )} */}
-                        {/* TODO: Work on loading state indicator */}
-                        {citySuggestions.map((city) => (
-                            <li 
-                                className="search-suggestion-location" 
-                                onClick={() => handleCitySuggestionClick(city)}
-                                key={`${city.latitude}-${city.longitude}`}
-                                // OR key={index} and add index as a param to the mapping function
-                            >
-                                <span className="suggestion-location-name">
-                                    {city.name}
-                                </span>
-                                <span className="suggestion-location-country">
-                                    {city.country}
-                                </span>
-                            </li>
-                        ))}
+                        {isSearching ? 
+                            (
+                                <div className="search-loading">
+                                    <Spinner />
+                                    <span className="search-loading-text">Search in progress</span>
+                                </div>
+                            )
+                        : 
+                            (
+                                citySuggestions.map((city) => (
+                                    <li 
+                                        className="search-suggestion-location" 
+                                        onClick={() => handleCitySuggestionClick(city)}
+                                        key={`${city.latitude}-${city.longitude}`}
+                                        // OR key={index} and add index as a param to the mapping function
+                                    >
+                                        <span className="suggestion-location-name">
+                                            {city.name}
+                                        </span>
+                                        <span className="suggestion-location-country">
+                                            {city.country}
+                                        </span>
+                                    </li>
+                                ))
+                            )
+                        }
                     </ul>
                 )}
 

9400cd34a3c27fecde1d3e66a70e546030ad22f1
Dugiii <dugidemeg@gmail.com>
2026-02-27 00:32:15 -0600

    created skeleton component

--- src/components/WeatherData.jsx
+++ src/components/WeatherData.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect, useCallback } from 'react'
 import { Search } from './Search';
-import { Spinner } from './Spinner';
 import { BASE_CITY_API_URL, BASE_WEATHER_API_URL, BDC_API_KEY, BDC_REVERSE_GEOCODING_API_URL } from '../api';
 import { weatherIcons } from '../weatherIcons';
 import { weatherDescriptions } from '../weatherDescriptions';
 import { DailyForecasts } from './DailyForecasts';
 import { DaysDropdown } from './DaysDropdown';
+import { Skeleton } from './Skeleton';
 import { useUnits } from '../context/UnitsContext';
 
 
@@ -338,81 +338,7 @@ export const WeatherData = () => {
             {isLoading || !weather.city ? 
                 (
                     // * Loading Skeleton
-                    <div className='weather-box'>
-                        <div className="weather-daily">
-
-                            {/* Skeleton: Location's Weather */}
-                            <div className="location-weather-skeleton">
-                                <div className="skeleton-loader">
-                                    <div className="loading-dots">
-                                        <span className='dot'></span>
-                                        <span className='dot'></span>
-                                        <span className='dot'></span>
-                                    </div>
-                                    <span className="skeleton-loader-text">
-                                        Loading...
-                                    </span>
-                                </div>
-                            </div>
-
-                            {/* Skeleton: Weather Details for Location */}
-                            <div className="location-weather-details">
-                                <div className="feels-like skeleton-card">
-                                    <p>Feels Like</p>
-                                    <span>
-                                        –
-                                    </span>
-                                </div>
-                                <div className="humidity skeleton-card">
-                                    <p>Humidity</p>
-                                    <span>
-                                        –
-                                    </span>
-                                </div>
-                                <div className="wind-speed skeleton-card">
-                                    <p>Wind Speed</p>
-                                    <span>
-                                        –
-                                    </span>
-                                </div>
-                                <div className="precipitation skeleton-card">
-                                    <p>Precipitation</p>
-                                    <span>
-                                        –
-                                    </span>
-                                </div>
-                            </div>
-
-                            {/* Skeleton: Daily Forecasts */}
-                            <div className="location-daily-forecasts">
-                                <p>Daily Forecast</p>
-                                <div className="days-forecasts">
-                                    {[...Array(7)].map((_, i) => (
-                                        <div className="day skeleton-day" key={i}></div>
-                                    ))}
-                                </div>
-                            </div>
-
-                        </div>
-
-                        <div className="weather-hourly">
-                            <div className="location-hourly-forecast skeleton-card">
-                                <div className="weekday-hourly-forecast">
-                                    <p>Hourly Forecast</p>
-                                    <div className="skeleton-days-dropdown">
-                                        <span>–</span>
-                                        <img src="/icon-dropdown.svg" alt="dropdown icon" />
-                                    </div>
-                                </div>
-
-                                <div className="hourly-weather-forecast h-[unset]">
-                                    {[...Array(24)].map((_, i) => (
-                                        <div className="skeleton-hourly-card" key={i}></div>
-                                    ))}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <Skeleton />
                 )
             :
                 // * Actual Weather Data

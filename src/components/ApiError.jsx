6e316e5609ede11ea6d402d26cde867a15f6bc57
Dugiii <dugidemeg@gmail.com>
2026-03-02 00:14:30 -0600

    retry handling

--- src/components/ApiError.jsx
+++ src/components/ApiError.jsx
@@ -1,10 +1,10 @@
-export const ApiError = () => {
+export const ApiError = ({ onRetry }) => {
     return (
         <div className="api-error-container">
             <img src="/icon-error.svg" alt="error icon" />
             <h1>Something Went Wrong</h1>
             <p>We couldn't connect to the server (API Error). Please try again in a few moments</p>
-            <button>
+            <button type="button" onClick={onRetry}>
                 <img src="/icon-retry.svg" alt="retry icon" />
                 Retry
             </button>

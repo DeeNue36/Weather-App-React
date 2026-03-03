c1817aadc1b536752a027a5a405409d08d31f450
Dugiii <dugidemeg@gmail.com>
2026-03-03 00:58:55 -0600

    imported date formatting functions

--- src/components/WeatherData.jsx
+++ src/components/WeatherData.jsx
@@ -8,6 +8,11 @@ import { weatherDescriptions } from '../weatherDescriptions';
 import { DailyForecasts } from './DailyForecasts';
 import { DaysDropdown } from './DaysDropdown';
 import { ApiError } from './ApiError';
+import { formatDate, 
+        formatDailyForecastDate, 
+        formatWeekday, 
+        formatHourlyForecastTime } 
+from '../utilities/dateFormatting';
 
 
 //? e. Conversion functions
@@ -233,48 +238,6 @@ export const WeatherData = () => {
             setIsLoading(false);
         }
     }, []);
-
-
-    //? f. Date and Time Formatting
-
-    //? f(i). Date Formatting for City/Location
-    const formatDate = (timeString) => {
-        const date = new Date(timeString);
-        return date.toLocaleDateString('en-US', {
-            weekday: 'long',
-            year: 'numeric',
-            month: 'short',
-            day: 'numeric'
-        });
-    };
-
-    //? f(ii). Date Formatting for Daily Forecast
-    const formatDailyForecastDate = (timeString) => {
-        const date = new Date(timeString);
-        return date.toLocaleDateString('en-US', {
-            weekday: 'short',
-        });
-    };
-
-    //? f(iii). Date and Time Formatting for Hourly Forecast
-
-    //? f(iii)(a) Date Formatting for Hourly Forecast
-    const formatWeekday = (timeString) => {
-        const date = new Date(timeString);
-        return date.toLocaleDateString('en-US', {
-            weekday: 'long',
-        });
-    };
-
-    //? f(iii)(b) Time Formatting for Hourly Forecast
-    const formatHourlyForecastTime = (timeString) => {
-        const date = new Date(timeString);
-        return date.toLocaleTimeString('en-US', {
-            hour: 'numeric',
-            hour12: true,
-            ampm: true
-        });
-    };
 
     //? g. First Render to get User Location and display weather data
     useEffect(() => {

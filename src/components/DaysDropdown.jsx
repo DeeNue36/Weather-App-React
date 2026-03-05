ac56fa626f357f8bc1fa2267ff0c69c3abe03caa
Dugiii <dugidemeg@gmail.com>
2026-03-04 23:00:10 -0600

    code comments and removed unused jsx

--- src/components/DaysDropdown.jsx
+++ src/components/DaysDropdown.jsx
@@ -2,12 +2,12 @@ import { useRef, useState, useEffect } from 'react';
 
 export const DaysDropdown = ({options, selected, onChange}) => {
     const [isOpen, setIsOpen] = useState(false);
-    const dropdownRef = useRef(null); // Reference to the dropdown element
+    const dropdownRef = useRef(null); // Ref of the dropdown element
 
-    // Close the dropdown when clicking outside
+    //? Close the dropdown when clicking outside
     useEffect(() => {
         const handleClickOutside = (event) => {
-            // "If ref has been assigned an element and that element is not the target of the event, close the dropdown i.e close the dropdown when clicking anywhere outside the dropdown"
+            // "If ref has been assigned an element and that element is not the targeted event by the user, close the dropdown i.e close the dropdown when clicking anywhere outside the dropdown"
             if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
                 setIsOpen(false);
             }
@@ -20,7 +20,7 @@ export const DaysDropdown = ({options, selected, onChange}) => {
         };
     }, []);
 
-    // Handle day option selection
+    //? Handle day option selection
     const handleOptionClick = (option) => {
         onChange(option);
         setIsOpen(false);
@@ -51,26 +51,3 @@ export const DaysDropdown = ({options, selected, onChange}) => {
         </div>
     );
 };
-    //     <div className="days-dropdown" ref={dropdownRef}>
-    //         <span 
-    //             className="days-dropdown-selected" 
-    //             onClick={() => setIsOpen(!isOpen)}
-    //         >
-    //             {selected || 'Select day'}
-    //             <img src="/icon-dropdown.svg" alt="dropdown icon" />
-    //         </span>
-    //         {isOpen && (
-    //             <ul className="days-dropdown-options">
-    //                 {options.map((option, index) => (
-    //                     <li
-    //                         key={index}
-    //                         className="day-dropdown-option"
-    //                         onClick={() => handleOptionClick(option)}
-    //                     >
-    //                         {option.longDate}
-    //                     </li>
-    //                 ))}
-    //             </ul>
-    //         )}
-    //     </div>
-    // );
